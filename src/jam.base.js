78e89ca0497eec229cce08bf029359c821d02a94
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2010-07-22 22:18:24 +0100

    add base object

---
+++ src/jam.base.js
@@ -0,0 +1,74 @@
+Jam.Base = function (name, options) {
+  var base = {};
+  var containerSelector = options.containerSelector || 'body';
+  var eventHandler = options.eventHandler || $(document);
+  var errorPrefix = 'Jam.Base';
+  var name = name;
+  var templateSelector = options.templateSelector;
+  var template = $(templateSelector);
+  var widget = options.widget;
+  var eventNamespace = function () {
+    return name + ':' + widget;
+  };
+
+  // bind a callback to an event on the objects event handler with the passed name and object namespace
+  base.bind = function (eventName, callback) {
+    eventHandler.bind(eventName + eventNamespace(), callback);
+    return this;
+  };
+
+  // return a namespaced class to be used on the objects markup
+  base.htmlClass = function (className) {
+    return name + ' ' + widget + '-' + className;
+  }
+
+  // select the container for this objects html
+  base.container = function () {
+    return $(containerSelector);
+  };
+
+  // generate the html for the object
+  base.generateHtml = function () {
+    return this.html;
+  };
+
+  base.html = template.clone();
+
+  // insert the objects html into the dom
+  base.insertHtml = function () {
+    this.container().append(this.html);
+  };
+
+  // generates the html for this object and inserts it into its container
+  // a customization function can be passed which will be called after generating
+  // the markup and before it is inserted into the dom, 
+  base.render = function (customization) {
+    var customization = customization || function () {};
+
+    this.generateHtml();
+    customization.call(this);
+    this.insertHtml();
+  };
+
+  // remove the objects html from the dom and unbind all events
+  base.remove = function () {
+    this.html.remove();
+    eventHandler.unbind(this.name + ':gridView')
+  };
+
+  // trigger an event 
+  base.trigger = function (eventName, data) {
+    eventHandler.trigger(eventName + eventNamespace(), data);
+  };
+
+  return base;
+}
+// 
+// 
+// Blah = function (collection) {
+//   var blah = {}
+// 
+//   $.extend(blah, new Jam.NewGridView({collection: collection, containerSelector: '#grid-holder', templateSelector: '#templates .test'}))
+// 
+//   return blah
+// }
